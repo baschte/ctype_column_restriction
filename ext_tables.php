34f676a57ff71d3aca29e43a92aeb75c47ab42ad
Sebastian Richter <sebastian.richter@pluswerk.ag>
2018-01-23 21:19:15 +0100

    [BUGFIX] only implement DragDrop JS in TYPO3 backend

--- ext_tables.php
+++ ext_tables.php
@@ -1,11 +1,13 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-/**
- * Register Hook for allowed column BE CTypes
- */
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms']['db_new_content_el']['wizardItemsHook'][] = \Baschte\CtypeColumnRestriction\Hooks\WizardItems::class;
+if (TYPO3_MODE === 'BE') {
+    /**
+     * Register Hook for allowed column BE CTypes
+     */
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms']['db_new_content_el']['wizardItemsHook'][] = \Baschte\CtypeColumnRestriction\Hooks\WizardItems::class;
 
-// DrawItemHook
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][] = \Baschte\CtypeColumnRestriction\Hooks\DrawItem::class;
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] = \Baschte\CtypeColumnRestriction\View\PageLayoutView::class.'->addJSCSS';
+    // DrawItemHook
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][] = \Baschte\CtypeColumnRestriction\Hooks\DrawItem::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] = \Baschte\CtypeColumnRestriction\View\PageLayoutView::class.'->addJSCSS';
+}
