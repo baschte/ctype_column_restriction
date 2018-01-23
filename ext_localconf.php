af6d94880be1bd34385e1e2fa39b2bf8ecf9bf3f
Sebastian Richter <sebastian.richter@pluswerk.ag>
2018-01-23 21:51:27 +0100

    [TASK] add condition for gridelements

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,8 +2,12 @@
 
 if (TYPO3_MODE === 'BE') {
 
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Backend\\View\\PageLayoutView'] = array(
-        'className' => 'Baschte\\CtypeColumnRestriction\\View\\PageLayoutView'
-    );
+    // only if gridelements is not installed
+    if(!TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('gridelements'))
+    {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Backend\\View\\PageLayoutView'] = array(
+            'className' => 'Baschte\\CtypeColumnRestriction\\View\\PageLayoutView'
+        );
+    }
 
 }
--- ext_tables.php
+++ ext_tables.php
@@ -2,12 +2,18 @@
 defined('TYPO3_MODE') || die();
 
 if (TYPO3_MODE === 'BE') {
-    /**
-     * Register Hook for allowed column BE CTypes
-     */
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms']['db_new_content_el']['wizardItemsHook'][] = \Baschte\CtypeColumnRestriction\Hooks\WizardItems::class;
 
-    // DrawItemHook
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][] = \Baschte\CtypeColumnRestriction\Hooks\DrawItem::class;
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] = \Baschte\CtypeColumnRestriction\View\PageLayoutView::class.'->addJSCSS';
+    // only if gridelements is not installed
+    if(!TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('gridelements'))
+    {
+        /**
+         * Register Hook for allowed column BE CTypes
+         */
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms']['db_new_content_el']['wizardItemsHook'][] = \Baschte\CtypeColumnRestriction\Hooks\WizardItems::class;
+
+        // DrawItemHook
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][] = \Baschte\CtypeColumnRestriction\Hooks\DrawItem::class;
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] = \Baschte\CtypeColumnRestriction\View\PageLayoutView::class.'->addJSCSS';
+    }
+
 }
